b4bd248aa15b193f379b992d3bacbffb6b2435f9
eliza <elizazijinhuang@gmail.com>
2019-04-06 09:03:05 -0400

    test entry

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,14 @@
-from media import app
+# from media import app
+#
+# if __name__ == '__main__':
+#     app.run()
 
-if __name__ == '__main__':
-    app.run()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
