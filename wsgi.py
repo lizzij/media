c8e18a3c6f51bc8cb3fc5bdee7cc17d9f53a861a
eliza <elizazijinhuang@gmail.com>
2019-04-09 17:07:21 -0400

    debug mode

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,5 @@
 from media import app
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
-
-# from flask import Flask
-# app = Flask(__name__)
-#
-# @app.route("/")
-# def hello():
-#     return "<h1 style='color:blue'>Hello There!</h1>"
-#
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0')
