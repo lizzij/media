f2a89e5656b7ccdaeeb8c0fd9d740e3b9f5eca58
eliza <elizazijinhuang@gmail.com>
2019-04-15 14:05:42 -0400

    survey page 3

---
+++ media/static/scripts/survey4.js
@@ -0,0 +1,20 @@
+function show(shown) {
+  var pages = ['page1', 'page2'];
+  var pageIndex;
+  for (pageIndex = 0; pageIndex < pages.length; pageIndex++) {
+    pageId = pages[pageIndex];
+    if (shown == pageId) {
+        document.getElementById(shown).style='display: flex;flex-direction: column;position: relative;';
+    }
+    else {
+      document.getElementById(pageId).style.display='none';
+    }
+  }
+  return 0;
+}
+
+function signUpFeeSlide() {
+  var value=document.getElementById("signUpFeeSlider").value;
+  var signUpFeeAmount = `${value}元`;
+  document.getElementById("signUpFeeAmount").value=signUpFeeAmount;
+}
