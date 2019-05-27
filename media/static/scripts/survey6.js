b56f13e3bfa3cfe08aa8014efb2235a380524dce
eliza <elizazijinhuang@gmail.com>
2019-05-26 20:20:35 -0400

    donation rule changed

--- media/static/scripts/survey6.js
+++ media/static/scripts/survey6.js
@@ -383,19 +383,20 @@ function hideWalkathonSlider() {
 }
 function showWalkathonSlider() {
   document.getElementById("walkathon").style.display = "block";
+  document.getElementById("walkathonAmount").style.paddingLeft = 5000 * 0.0172 + "px";
 }
 
 function walkathonSlide() {
-  var value=document.getElementById("walkathonSlider").value;
-  var walkathonAmount = `${value}步`;
-  var distance = value * 0.0008;
-  var donation = (value - 4000) * 0.01;
-  distance = distance.toFixed(2);
-  donation = donation.toFixed(2);
-  document.getElementById("walkathonAmount").value=walkathonAmount;
-  document.getElementById("walkathonDistance").value=`走${value}步（${distance}公里）\n—— 研究人员将代表您向上海联合基金会捐赠${donation}元人民币。`;
-  var left = 0.1 * value - 435;
-  document.getElementById("walkathonAmount").style.paddingLeft = left + "px";
+var value=document.getElementById("walkathonSlider").value;
+var walkathonAmount = `${value}步`;
+var distance = value * 0.0008;
+var donation = value * 0.002;
+distance = distance.toFixed(2);
+donation = donation.toFixed(2);
+document.getElementById("walkathonAmount").value=walkathonAmount;
+document.getElementById("walkathonDistance").value=`走${value}步（${distance}公里）\n—— 研究人员将代表您向上海联合基金会捐赠${donation}元人民币。`;
+var left = value * 0.0172;
+document.getElementById("walkathonAmount").style.paddingLeft = left + "px";
 }
 
 // function updateWalkathonSlide(value) {
