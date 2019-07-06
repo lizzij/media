120c71869e2f7d9c9a04f67541b98a9d861ba4b1
eliza <elizazijinhuang@gmail.com>
2019-07-06 19:17:16 -0400

    containerize pilot (cohort 1, 2)

--- media/__init__.py
+++ media/__init__.py
@@ -27,7 +27,7 @@ def create_app(test_config=None):
     @app.route('/')
     @app.route('/home')
     def home():
-        return render_template('home.html')
+        return render_template('crud/home.html')
 
     from media import db
     db.init_app(app)
