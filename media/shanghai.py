db01fbf4a82c87b8772c61cbe1cf8bbc5aa74760
eliza <elizazijinhuang@gmail.com>
2019-08-25 14:07:20 -0400

    add entry point

--- media/shanghai.py
+++ media/shanghai.py
@@ -12,3 +12,53 @@ from media.db import get_db
 from werkzeug.exceptions import abort
 
 bp = Blueprint("shanghai", __name__, url_prefix="/shanghai")
+
+def get_user(user_id_hashid, day_hashid, cohort = 4):
+    user = get_db().execute(
+        'SELECT user_id, day, treatment'
+        ' FROM user u'
+        ' WHERE u.user_id_hashid = ? AND u.day_hashid = ? AND u.cohort = ?',
+        (user_id_hashid, day_hashid, cohort,)
+    ).fetchone()
+    if user is None:
+        abort(404, "User {0}/{1}/{2} doesn't exist.".format(user_id_hashid, day_hashid, cohort))
+    else:
+        return user
+
+def get_event_info(event_id, cohort = 4):
+    info = get_db().execute(
+        'SELECT i.event_id,title,subtitle,info_date,info_time,location,image_file,short_description,low_temp,high_temp,suitable_for_family,suitable_for_friends,suitable_for_lover,suitable_for_baby,suitable_for_elderly,suitable_for_pet,event_details,phrase_for_week, phrase_for_day, phrase_for_header'
+        ' FROM infos i'
+        ' WHERE i.event_id = ? AND cohort = ?',
+        (event_id, cohort,)
+    ).fetchone()
+    return info
+
+def get_lastpage(user_id, day, day_to_lastpage_dict = {1:6, 2:9}): # excluding the info & final page
+    db = get_db()
+    last_activity = db.execute(
+        'SELECT survey_page, day'
+        ' FROM activity a'
+        ' WHERE a.user_id = ?',
+        (user_id,)
+    ).fetchone()
+    if last_activity is None:
+        lastpage = 0
+    else:
+        lastday = last_activity[1]
+        if day < lastday: # completed
+            lastpage = day_to_lastpage_dict[day]
+        elif day == lastday: # partially completed
+            lastpage = last_activity[0]
+        else:
+            lastpage = 0
+    return lastpage
+
+def update_lastpage(lastpage, day_complete, user_id, day):
+    now = datetime.now()
+    db = get_db()
+    db.execute(
+        'UPDATE activity SET survey_page = ?, curr_time = ?, day_complete = ? WHERE user_id = ? AND day = ?',
+        (lastpage, now, day_complete, user_id, day,)
+    )
+    db.commit()
