877575a3e878cb250102b0b5605d1c93264c2e07
eliza <elizazijinhuang@gmail.com>
2019-08-30 08:08:13 -0400

    record wrong answers

--- media/static/scripts/shanghai7.js
+++ media/static/scripts/shanghai7.js
@@ -356,16 +356,17 @@ function checkBeanNumberSource(){
   var AtoldMe = document.getElementById("AtoldMe").checked;
   var BtoldMe = document.getElementById("BtoldMe").checked;
   var CtoldMe = document.getElementById("CtoldMe").checked;
+  var wongAnswers = document.getElementById("whoToldMeWrongAnswers").value;
   if (AtoldMe && (!BtoldMe) && CtoldMe) {
     document.getElementById("beanNumberResult").innerHTML = '正确';
     document.getElementById("beanNumberResult").style.color = "#28a745";
     document.getElementById("beanCountEstimateQuestion").style.display = "block";
-
   }
   else {
     document.getElementById("beanNumberResult").innerHTML = '您的答案错误。 请再检查一次！';
     document.getElementById("beanNumberResult").style.color = "#FF3333";
-    document.getElementById("beanCountEstimateQuestion").style.display = "none";
+    var updatedWrongAns = wongAnswers.concat('(' + (AtoldMe ? 'AtoldMe ' : '') + (BtoldMe ? 'BtoldMe ' : '') + (CtoldMe ? 'CtoldMe ' : '') + ')');
+    document.getElementById("whoToldMeWrongAnswers").value = updatedWrongAns;
   }
 }
 
