a8ce80839c5a602acc7b7a5cc9081f746cd19af3
eliza <elizazijinhuang@gmail.com>
2019-09-05 16:55:07 -0400

    add error logger

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,12 @@
 from media import app
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+@app.route('/')
+def hello_world():
+    app.logger.info('Processing default request')
+    return 'Hello World!'
 
 if __name__ == '__main__':
     app.debug = True
